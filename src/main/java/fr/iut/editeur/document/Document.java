bda3904597cba4f68cf02221d07664727f349939
El-Hadi Omar <omar.el-hadi@etu.umontpellier.fr>
2024-09-10 16:24:20 +0200

    Commande Majuscule

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -30,6 +30,12 @@ public class Document {
         }
     }
 
+    public void majuscules(int debut, int fin) {
+        String partie = texte.substring(debut, fin);
+        partie =partie.toUpperCase();
+        remplacer(debut, fin-1, partie);
+    }
+
     @Override
     public String toString() {
         return this.texte;
